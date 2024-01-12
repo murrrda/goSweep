e7f9c49cdac05d2ef724b1b1aa92e2d639413014
murrrda <aleksamrda@gmail.com>
2024-01-12 23:51:24 +0100

    Integrated ICMP Echo Request and Reply handling for a basic ping command

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module goSweep
 
 go 1.21.5
+
+require (
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,74 @@
 package main
 
 import (
-    "fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
+// ip to ping
+const ip = "8.8.8.8"
+
 func main() {
-    fmt.Println("Hello, world!")
+    log.Println("Hello, world!")
+
+    pingIP(ip)
+}
+
+
+func pingIP(ip string) (string, error) {
+    // resolving Ip addr
+    ipAddr, err := net.ResolveIPAddr("ip", ip)
+    if err != nil {
+        log.Fatal("Error resolving IP: ", err)
+    }
+
+    // creating socket
+    conn, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+    if err != nil {
+        log.Fatal("Error creating a socket: ", err)
+    }
+    defer conn.Close()
+
+    // creating ICMP message
+    message := icmp.Message {
+        Type:     ipv4.ICMPTypeEcho,
+        Code:     0,
+        Body:     &icmp.Echo {
+            ID:   os.Getpid() & 0xffff,
+            Seq:  1,
+            Data: []byte("Hello-Friend!"),
+        },
+    }
+
+    // converting message to bytes (Marshaling)
+    messageByted, err := message.Marshal(nil)
+    if err != nil {
+        log.Fatal("Error Marshaling ICMP message: ", err)
+    }
+
+    startTime := time.Now()         // start counting time
+    // send ICMP package
+    _, err = conn.WriteTo(messageByted, ipAddr)
+    if err != nil {
+        log.Fatal("Error sending ICMP package: ", err)
+    }
+
+    replyBuffer := make([]byte, 1500)
+
+    // receiving ICMP package
+    _, _, err = conn.ReadFrom(replyBuffer)
+    if err != nil {
+        log.Fatal("Error receiving ICMP reply: ", err)
+    }
+
+    latency := time.Since(startTime)
+
+    log.Println(latency)
+
+    return "", err
 }
