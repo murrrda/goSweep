2df8f309d2619b4655718b116fb4126792de2659
murrrda <aleksamrda@gmail.com>
2024-01-15 00:23:52 +0100

    fix: handling unreachable hosts

--- main.go
+++ main.go
@@ -4,30 +4,65 @@ import (
     "log"
     "net"
     "os"
-    "strings"
+    //"sync"
     "time"
     "strconv"
+    "strings"
 
     "golang.org/x/net/icmp"
     "golang.org/x/net/ipv4"
 )
 
+
 func main() {
-    if len(os.Args) != 2 {
-        log.Fatal("You should provide IP address")
-    }
-    targetAddr := os.Args[1]
-
-    if strings.Count(targetAddr, "x") > 0 {
-        targetAddr = strings.Replace(targetAddr, "x", strconv.FormatInt(int64(0), 10), -1)
-        pingIP(targetAddr)
-    }
-        pingIP(targetAddr)
-    //for _, v := range []int{0, 256} {
-
+    //if len(os.Args) != 2 {
+    //    log.Fatal("You should provide IP address in CIDR notation")
     //}
+    //targetAddr := os.Args[1]
+    ip := "192.168.0.20"
+
+    //ip, ipNet, err := net.ParseCIDR(targetAddr)
+    //if err != nil {
+    //    log.Fatal("Error parsing -- please provide input in CIDR notation: 192.168.0.1/24 for example")
+    //}
+
+    for i := 20; i < 256; i++ {
+        ipParts := strings.Split(ip, ".")
+
+        ipParts[3] = strconv.Itoa(i)
+
+        newIp := strings.Join(ipParts, ".")
+        pingIP(newIp)
+    }
+
+    //var wg sync.WaitGroup
+    //// Ping all IP addresses in the network
+    //for ip := ip.Mask(ipNet.Mask); ipNet.Contains(ip); incrementIP(ip) {
+    //    ipStr := ip.String()
+    //    wg.Add(1)
+    //    go func(ipStr string) {
+    //        defer wg.Done()
+
+    //        _, err := pingIP(ipStr)
+    //        if err != nil {
+    //            log.Println(err)
+    //        } else {
+    //            //log.Println(result)
+    //        }
+    //    }(ipStr)
+    //}
+    //// Wait for all goroutines to finish
+    //wg.Wait()
 }
 
+func incrementIP(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
 
 func pingIP(ip string) (string, error) {
     // resolving Ip addr
@@ -75,16 +110,29 @@ func pingIP(ip string) (string, error) {
     replyBuffer := make([]byte, 1500)
 
     // receiving ICMP reply
-    _, _, err = conn.ReadFrom(replyBuffer)
+    icmpInt, icmpNetAddr, err := conn.ReadFrom(replyBuffer)
     if err != nil {
-        log.Println("No reply (%v): %v", ip, err)
+        log.Printf("No reply (%v): %v\n", ip, err)
         return "", err
     }
 
-    latency := time.Since(startTime)
+    // Parse the received ICMP packet
+    receivedMsg, err := icmp.ParseMessage(1, replyBuffer[:icmpInt])
+    if err != nil {
+        log.Println("Error parsing ICMP reply: ", err)
+        return "", err
+    }
 
-    log.Println(ip)
-    log.Println(latency)
+    // Check if the received packet is an echo reply
+    switch receivedMsg.Type {
+    case ipv4.ICMPTypeEchoReply:
+        log.Printf("Received reply from %v: %v\n", ip, icmpNetAddr)
+        // Further processing or return as needed
+    default:
+        log.Printf("Received unexpected ICMP type %v from %v\n", receivedMsg.Type, ip)
+    }
+
+    latency := time.Since(startTime)
 
     return ip + " time: " +latency.String(), nil
 }
